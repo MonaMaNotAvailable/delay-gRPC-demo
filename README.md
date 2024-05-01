0ab64f3c3fb40ee72350cbe917e295890b24532a
MonaMaNotAvailable <zma4@wpi.edu>
2024-05-01 17:39:00 -0400

    add client side interceptor

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # delay-gRPC-demo
+
 Keep a copy of my master thesis
+
+## Commands
+* Generate stubs of gRPC  
+
+```
+python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services.proto
+```
+
+## Notes
+* Make sure all servers listening on port 50051 are properly terminated!!!
--- client.py
+++ client.py
@@ -2,22 +2,48 @@ import grpc
 import asyncio
 import services_pb2
 import services_pb2_grpc
+import logging
+from typing import Callable, Awaitable
 
-import grpc
-import asyncio
-import services_pb2
-import services_pb2_grpc
+class ClientSideDelayInterceptor(grpc.aio.UnaryUnaryClientInterceptor):
+    def __init__(self, delay_ms: int, service_ids: list[str]) -> None:
+        #The delay is introduced after the response is received from the server
+        self.delay_ms = delay_ms / 1000.0  # Convert milliseconds to seconds
+        self.service_ids = service_ids
+
+    async def intercept_unary_unary(
+        self, continuation: Callable, client_call_details: grpc.aio.ClientCallDetails, request
+    ) -> grpc.aio.Call:
+        #Intercepts the unary-unary client call to introduce a delay after receiving the server's response.
+        
+        response = await continuation(client_call_details, request)
+        
+        # Check the service ID in the metadata
+        service_id = next((value for key, value in client_call_details.metadata if key == 'service-id'), None)
+        print(service_id)
+
+        if service_id in self.service_ids:
+            logging.info(f"Introducing delay after receiving response for service {service_id} by {self.delay_ms} seconds")
+            await asyncio.sleep(self.delay_ms)  # Introduce an asynchronous delay after receiving the response
+        return response
 
 async def handle_recommendation(stub):
-    response = await stub.Recommend(services_pb2.RecommendationRequest(user_id='1'))
+    service_id = "recommendationService"
+    metadata = grpc.aio.Metadata(("service-id", service_id),)
+    response = await stub.Recommend(services_pb2.RecommendationRequest(user_id='1'), metadata = metadata)
     print("Recommendations: ", response.recommendations)
 
 async def handle_restaurant(stub):
-    response = await stub.GetRestaurants(services_pb2.RestaurantRequest(query=''))
+    service_id = "restaurantService"
+    metadata = grpc.aio.Metadata(("service-id", service_id),)
+    response = await stub.GetRestaurants(services_pb2.RestaurantRequest(query=''), metadata = metadata)
     print("Restaurants: ", response.names)
 
 async def run():
-    async with grpc.aio.insecure_channel('localhost:50051') as channel:
+    interceptors = [
+        ClientSideDelayInterceptor(5000, ["testService1", "recommendationService"])  # Delays processing by 5000 milliseconds = 5 seconds
+    ]
+    async with grpc.aio.insecure_channel('localhost:50051', interceptors=interceptors) as channel:
         recommendation_stub = services_pb2_grpc.RecommendationServiceStub(channel)
         restaurant_stub = services_pb2_grpc.RestaurantCatalogServiceStub(channel)
 
@@ -31,61 +57,3 @@ async def run():
 
 if __name__ == '__main__':
     asyncio.run(run())
-
-
-
-# async def recommendationAndRestaurant():
-#     async with grpc.aio.insecure_channel('localhost:50051') as channel:
-#         recommendation_stub = services_pb2_grpc.RecommendationServiceStub(channel)
-#         restaurant_stub = services_pb2_grpc.RestaurantCatalogServiceStub(channel)
-
-#         # For concurrent execution
-#         recommendation_task = recommendation_stub.Recommend(services_pb2.RecommendationRequest(user_id='1'))
-#         restaurant_task = restaurant_stub.GetRestaurants(services_pb2.RestaurantRequest(query=''))
-
-#         # Execute
-#         recommendation_response, restaurant_response = await asyncio.gather(recommendation_task, restaurant_task)
-
-#         print("Recommendations: ", recommendation_response.recommendations)
-#         print("Restaurants: ", restaurant_response.names)
-
-# if __name__ == '__main__':
-#     asyncio.run(recommendationAndRestaurant())
-
-
-
-# import grpc
-# import asyncio
-# import services_pb2
-# import services_pb2_grpc
-# from opentelemetry import trace
-# from opentelemetry.instrumentation.grpc import GrpcInstrumentorClient
-# from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-# from opentelemetry.sdk.trace import TracerProvider
-# from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
-# def init_tracer():
-#     trace.set_tracer_provider(TracerProvider())
-#     tracer_provider = trace.get_tracer_provider()
-#     otlp_exporter = OTLPSpanExporter(endpoint="localhost:4317", insecure=True)
-#     span_processor = BatchSpanProcessor(otlp_exporter)
-#     tracer_provider.add_span_processor(span_processor)
-
-# async def run():
-#     init_tracer()
-#     GrpcInstrumentorClient().instrument()
-
-#     async with grpc.aio.insecure_channel('localhost:50051') as channel:
-#         recommendation_stub = services_pb2_grpc.RecommendationServiceStub(channel)
-#         restaurant_stub = services_pb2_grpc.RestaurantCatalogServiceStub(channel)
-
-#         recommendation_task = recommendation_stub.Recommend(services_pb2.RecommendationRequest(user_id='1'))
-#         restaurant_task = restaurant_stub.GetRestaurants(services_pb2.RestaurantRequest(query=''))
-
-#         responses = await asyncio.gather(recommendation_task, restaurant_task)
-
-#         print("Recommendations: ", responses[0].recommendations)
-#         print("Restaurants: ", responses[1].names)
-
-# if __name__ == '__main__':
-#     asyncio.run(run())
