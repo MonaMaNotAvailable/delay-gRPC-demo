7a50745c4368bcb8c3897db4f9d0f6a8c940d4e4
MonaMaNotAvailable <zma4@wpi.edu>
2024-05-08 11:51:40 -0400

    add sequential mode

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Keep a copy of my master thesis
 
 ## Commands
-* Generate stubs of gRPC  
+* Generate stubs of gRPC in Python
 
 ```
 python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services.proto
@@ -11,3 +11,8 @@ python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. services.pro
 
 ## Notes
 * Make sure all servers listening on port 50051 are properly terminated!!!
+
+
+## Python
+* [gRPC Interceptors](https://grpc.github.io/grpc/python/grpc_asyncio.html#grpc.aio.ClientInterceptor)
+* Challenge: Global Interpreter Lock (GIL)
--- client.py
+++ client.py
@@ -48,21 +48,36 @@ async def handle_restaurant(stub):
     print("Restaurants: ", response.names)
     print(f"Request took approximately {elapsed_time:.6f} seconds.")
 
-async def run():
+async def run(mode):
     interceptors = [
-        ClientSideDelayInterceptor(5000, ["testService1", "recommendationService"])  # Delays processing by 5000 milliseconds = 5 seconds
+        ClientSideDelayInterceptor(0, ["testService1", "recommendationService"])  # Delays processing by 5000 milliseconds = 5 seconds
     ]
     async with grpc.aio.insecure_channel('localhost:50051', interceptors=interceptors) as channel:
         recommendation_stub = services_pb2_grpc.RecommendationServiceStub(channel)
         restaurant_stub = services_pb2_grpc.RestaurantCatalogServiceStub(channel)
 
-        # Create tasks for both RPC calls
-        task1 = asyncio.create_task(handle_recommendation(recommendation_stub))
-        task2 = asyncio.create_task(handle_restaurant(restaurant_stub))
+        if mode == 'concurrent':
+            # Create tasks for both RPC calls
+            task1 = asyncio.create_task(handle_recommendation(recommendation_stub))
+            task2 = asyncio.create_task(handle_restaurant(restaurant_stub))
 
-        # Wait for both tasks to complete
-        await task1
-        await task2
+            # Wait for both tasks to complete
+            await asyncio.gather(task1, task2)
+            # await task1
+            # await task2
+
+        elif mode == 'sequential':
+            await handle_recommendation(recommendation_stub)
+            await handle_restaurant(restaurant_stub)
 
 if __name__ == '__main__':
-    asyncio.run(run())
+    try:
+        mode = int(input("Select a mode (0 -> concurrent, 1 -> sequential): "))
+        if mode == 0:
+            asyncio.run(run('concurrent'))
+        elif mode == 1:
+            asyncio.run(run('sequential'))
+        else:
+            raise ValueError("Invalid Input!!! Please choose 0 for concurrent or 1 for sequential mode!")
+    except ValueError as e:
+        print(f"Error: {e}")
